ff5c74ac0ae78b7603c46e6a8ea8442f67d1e74b
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-02-26 12:30:11 -0500

    updated server file functions

--- server/server.js
+++ server/server.js
@@ -1,34 +1,41 @@
-const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
-const { ApolloServer } = require("apollo-server-express");
+const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
 
-const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
+const db = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+require('dotenv').config();
 
-server.applyMiddleware({ app });
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+app.use(routes);
 
-db.once("open", () => {
+db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
