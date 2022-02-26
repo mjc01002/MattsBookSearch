8d91cb18131967c21a9f054b79bd8b0aa14db972
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-02-26 11:45:44 -0500

    updated for ticket

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
-// const { param } = require('../routes');
+
 
 const resolvers = {
 
