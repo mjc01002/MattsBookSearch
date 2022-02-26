b1b5cba5b2bc9bd4bb49b6c14c51c1c4f54645a2
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-02-26 13:15:17 -0500

    updated server to use graphql routes

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
